9b41ed488ad702d0bd4705fd7824a16c83731448
guomanyi <guomanyi@jimi360.cn>
2020-06-17 14:54:41 +0800

    jmlauchad

---
+++ jmlauchad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  jmlauchad
+//
+//  Created by JM on 2020/6/17.
+//  Copyright © 2020 JM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ jmlauchad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jmlauchad
+//
+//  Created by JM on 2020/6/17.
+//  Copyright © 2020 JM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
